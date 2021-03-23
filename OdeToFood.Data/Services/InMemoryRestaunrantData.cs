90787cc8a136d707e40a020b41080e303db1ed55
Oluwafemi Ajayi <aby4reality@gmail.com>
2021-03-23 01:58:44 +0100

    Add project files.

---
+++ OdeToFood.Data/Services/InMemoryRestaunrantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaunrantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaunrantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "aby's Pizza", Cuisine = CuisineType.French},
+                new Restaurant {Id = 1, Name = "Tersiguels", Cuisine = CuisineType.Chineese},
+                new Restaurant {Id = 1, Name = "Mango Groove", Cuisine = CuisineType.Italaian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
